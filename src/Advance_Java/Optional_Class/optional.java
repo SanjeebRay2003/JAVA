595251f7c5f843832a4f0c4f31ab7e3487b05ba4
SanjeebRay2003 <sanjeebray2003@gmail.com>
2024-12-23 20:23:41 +0530

    Optional

--- src/Advance_Java/Optional_Class/optional.java
+++ src/Advance_Java/Optional_Class/optional.java
@@ -1,12 +1,26 @@
 package Advance_Java.Optional_Class;
 
+import java.util.List;
 import java.util.Optional;
 
 public class optional {
+
+    public static Optional<Integer> hello(){ // optional method
+        Integer a =null;
+        return Optional.ofNullable(a);
+    }
+
     public static void main(String[] args) {
 
-        Optional<Integer> val = Optional.ofNullable(45);
-        System.out.println(val.isPresent());
+        Integer num = null ;
+
+        Optional<Integer> val = Optional.ofNullable(num);
+        System.out.println(val.isPresent()); // returns boolean
+//        System.out.println(val.get()); // throws NoSuchElementException if value is null
+        System.out.println(val.orElse(10)); // enters the value if it is not present ot null
+
+        Optional<Integer> b = hello();
+        System.out.println(b.orElse(100));
 
     }
 }
